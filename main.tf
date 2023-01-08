6970e5156c0429e6818d14516068d5e562deb5e8
prajwal.t.jayakumar <prajwal.t.jayakumar@DX-20220401-03.local>
2023-01-08 12:45:12 +0900

    removed duplicates

--- main.tf
+++ main.tf
@@ -37,34 +37,11 @@ resource "aws_security_group" "alexa" {
   name = join("-",["alexa-aws-sg",tostring(timestamp())])
 }
 
-resource "aws_instance" "tfvm" {
-  ami = "ami-0885b1f6bd170450c"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.websg.id ]
-  user_data = <<-EOF
-                #!/bin/bash
-                echo "I LOVE TERRAFORM" > index.html
-                nohup busybox httpd -f -p 8080 &
-                EOF
-    tags = {
-      Name = "WEB-demo"
-    }
-}
-resource "aws_security_group" "websg" {
-  name = "web-sg01"
-  ingress {
-    protocol = "tcp"
-    from_port = 8080
-    to_port = 8080
-    cidr_blocks = [ "0.0.0.0/0" ]
-  }
+  # lifecycle {
+  #   # Necessary if changing 'name' or 'name_prefix' properties.
+  #   create_before_destroy = true
+  # }
 
-  lifecycle {
-    # Necessary if changing 'name' or 'name_prefix' properties.
-    create_before_destroy = true
-  }
-
-}
 output "instance_ips" {
   value = aws_instance.tfvm.public_ip
 }
