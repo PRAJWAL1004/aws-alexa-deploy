421f8309808f68a55a837cc7448034dd9c00431f
prajwal.t.jayakumar <prajwal.t.jayakumar@DX-20220401-03.local>
2023-01-08 12:56:51 +0900

    running through shell script

--- main.tf
+++ main.tf
@@ -41,6 +41,7 @@ resource "aws_instance" "tfvm" {
 }
 resource "aws_security_group" "alexa" {
   name = join("-",["alexa-aws-sg",tostring(timestamp())])
+  vpc_id = ["${aws_vpc.my_alexa_vpc}"]
 }
 
   # lifecycle {
